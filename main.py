0a64df3dbea2a1a642dadff10eb8f6eb78deb4dc
JKramer91 <jonkra91@hotmail.com>
2024-07-04 14:32:15 +0200

    Done!

--- main.py
+++ main.py
@@ -22,12 +22,25 @@ def character_count(book):
             char_dict[char] = 1
     return char_dict
 
+def sort_on(dict):
+    return dict["num"]
+
+def dict_to_sorted_list(dict):
+    dict_list = []
+    for char, num in dict.items():
+        new_dict = {'char': char, 'num': num}
+        dict_list.append(new_dict)
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+
 def print_report(path, book, dict):
+    dict_list = dict_to_sorted_list(dict)
     print("--- Begin report of " + path + " ---\n" +
-          str(number_of_words(book)) + " words found in the document")
-
-
-
-
+          str(number_of_words(book)) + " words found in the document\n")
+    for item in dict_list:
+        if item["char"].isalpha():
+            print("The '" + item["char"] + "' character was found " + str(item["num"]) + " times." )
+    print("--- End report ---")
 
 main()
